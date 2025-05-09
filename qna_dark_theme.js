68f6ea349596cbd2d615b917ca8ef60f81c324f1
dim5x <dim5x@yahoo.com>
2025-05-09 11:56:53 +0300

    fix меню редактирования вопроса.

--- qna_dark_theme.js
+++ qna_dark_theme.js
@@ -1,18 +1,59 @@
+// ==UserScript==
+// @name         Тёмная тема для qna.habr.com
+// @namespace    http://tampermonkey.net/
+// @version      2025-05-09
+// @description  Тёмная тема для qna.habr.com
+// @author       dim5x
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=habr.com
+// @match        https://qna.habr.com/*
+// @grant        GM_addStyle
+// @homepageURL  https://github.com/dim5x/qna_dark_theme
+// @updateURL    https://raw.githubusercontent.com/dim5x/qna_dark_theme/refs/heads/master/qna_dark_theme.js
+// @downloadURL  https://raw.githubusercontent.com/dim5x/qna_dark_theme/refs/heads/master/qna_dark_theme.js
+// @supportURL   https://github.com/dim5x/qna_dark_theme/issues
+// ==/UserScript==
+
+
 (function() {
     'use strict';
 
     GM_addStyle(`
 /* ================= Общие стили ================= */
-/*:root {
-  --dark-bg: #505b6e; /*Общий фон*/
-  --dark-text: #e0e0e0;
-  --light-bg: #e0e0e0;
-  --light-text: #000;
-  --accent-green: #65c178;
-  --input-bg: #3a4457;
-  --code-bg: #CED4D4;
-  --hover-bg: #eef2f5;
-}*/
+:root {
+    --dark-bg: #505b6e; /*Общий фон*/
+    --dark-text: #e0e0e0;
+    --light-bg: #e0e0e0;
+    --light-text: #000;
+    --accent-green: #65c178;
+    --input-bg: #3a4457;
+    --code-bg: #CED4D4;
+    --hover-bg: #eef2f5;
+}
+
+::-webkit-scrollbar {
+    width: 14px; /* Общая ширина с учетом отступов */
+    height: 12px; /* Общая высота с учетом отступов */
+}
+
+::-webkit-scrollbar-track {
+    background: #ced4d4;
+    border: solid 1px black;
+    margin: 2px; /* Отступ трека от краев */
+}
+
+::-webkit-scrollbar-thumb {
+    background: #515c6e;
+    border-radius: 3px;
+    border: 2px solid transparent; /* Прозрачная граница создает отступ */
+    background-clip: content-box; /* Ограничивает фон только внутренней областью */
+}
+
+::-webkit-scrollbar-thumb:hover {
+    background: #505E5E;
+    border: 2px solid transparent;
+    background-clip: content-box;
+}
+
 /* ------------------------- */
 /* Верхнее левое меню "Хабр" */
 /* ------------------------- */
@@ -23,53 +64,48 @@
 
 /* Меняем цвет SVG элементов */
 path {
-    fill: #fff; /* Белый цвет */
+    fill: #e0e0e0; /* Белый цвет */
 }
 
 /* ----------end------------ */
 
-header, a.service,
-div.layout__body::before,
+header,
 aside,
+button,
+a.service,
+div.layout__body::before,
 dl, dd, dt,
 .page_header {
-    background-color: #505b6e !important;
+    background: #505b6e !important;
     color: #fff !important;
 }
 
-/*Поиск*/
-/*Серый.*/
+nav {
+    background: #505b6e !important;
+    color: #000 !important;
+}
+
+/*Поиск. #3a4457 - серый цвет.*/
 input {
-     background-color: #3a4457 !important;
-}
-
-.suggest__field {
-color: #CED4D4 !important;
-}
-
-/*Все результаты по запросу*/
-.ui-menu-item strong {
-   color: #fff !important;
+    background: #3a4457 !important;
 }
 
 
 .ui-menu-item a:hover {
-    background-color: #CED4D4 !important;
+    background: #CED4D4 !important;
     color: #505b6e !important;
 }
 
 
-ul {
-    background-color: #505b6e !important;
+ul,
+.form_comments, /* Рамка вокруг поля комментариев */
+.events-list__item.event_navbar {
+    background: #505b6e !important;
 }
 
-nav {
-    background-color: #505b6e !important;
-    color: #000 !important;
-}
 
 a.filters-menu__link {
-    background-color: #505b6e !important;
+    background: #505b6e !important;
     color: #e0e0e0 !important;
     border: none;
 }
@@ -81,11 +117,10 @@ button.btn.btn_outline_grey,
 .btn_solution,
 .btn_like,
 .btn_complexity-filter,
-.filters-menu.filters-menu_mobile
-{
+.filters-menu.filters-menu_mobile {
     color: #71c07b !important;
     border: 1px solid !important;
-    border-color:  #71c07b !important;
+    border-color: #71c07b !important;
 }
 
 button.btn.btn_subscribe:hover,
@@ -93,15 +128,14 @@ button.btn.btn_outline_grey:hover,
 button.btn.btn_outline_green:hover,
 .btn_solution:hover,
 .btn_like:hover,
-.btn_complexity-filter:hover
-{
+.btn_complexity-filter:hover {
     border-color: #369249 !important;
-    background-color: #505b6e !important;
+    background: #505b6e !important;
 }
 
 .filters-menu__link_active {
-border: 2px solid !important;
-    border-color:  #71c07b !important;
+    border: 2px solid !important;
+    border-color: #71c07b !important;
 }
 
 /* To-do Кнопка закрыть при выборе сложности вопросов */
@@ -110,16 +144,25 @@ border: 2px solid !important;
 }
 
 
-
 /* Альтернативный ник */
 .user-summary__nickname {
-    color: #CED2D4;
+    color: #CED2D4 !important;
 }
 
+/*       Теги в "Задать вопрос"         */
+.ui-widget-content.ui-autocomplete-input {
+    background-color: #515c6e !important;
+    color: #e0e0e0 !important;
+    border: solid 1px #e0e0e0;
+}
 
+a.tagit-close .text-icon,
+.tagit-label {
+    color: #e0e0e0 !important
+}
 
 /* Цвет отредактированного вопроса, который принял модератор */
-.question__title-link{
+.question__title-link {
     color: #CFD6E3 !important;
 }
 
@@ -127,44 +170,37 @@ border: 2px solid !important;
 /*        Задать вопрос             */
 /* -------------------------------  */
 #question_title {
-    background-color: #515c6e !important;
+    background: #515c6e !important;
     color: #e0e0e0
 }
 
-.ui-widget-content.ui-autocomplete-input {
-    background-color: #ced4d4 !important;
-}
-
-
 .page__header-title.page__header-title_bordered,
 .field__label,
-.tagit-label {
-    color: #fff !important;
+.ui-menu-item strong, /*Все результаты по запросу*/
+.fieldset__title, .fieldset__notice-type /* Настройка уведомлений */
+{
+    color: #e0e0e0 !important;
 }
 
-.selectBox-options>li.selectBox-hover a {
-	background-color: #e0e0e0 !important;
-	color: #000 !important;
+.selectBox-options > li.selectBox-hover a {
+    background: #e0e0e0 !important;
+    color: #000 !important;
 }
 
 .ui-autocomplete.ui-front.ui-menu.ui-widget.ui-widget-content.ui-corner-all.utocomplete_tags {
-	background-color: #e0e0e0 !important;
-	color: #000 !important;
+    background: #e0e0e0 !important;
+    color: #000 !important;
 }
+
 /* --------------end-----------------  */
 
-
-/* Настройка уведомлений */
-.fieldset__title, .fieldset__notice-type {
-    color: #fff !important;
-}
 
 /* ----------------------------------- */
 /*           Код, подсветка            */
 /* ----------------------------------- */
 pre, code, textarea {
     border: solid 2px #000;
-    background-color: #CED4D4 !important;
+    background: #CED4D4 !important;
     color: #000 !important;
 }
 
@@ -172,22 +208,19 @@ textarea {
     border: none;
 }
 
-/* Рамка вокруг поля комментариев */
-.form_comments {
-    background-color: #505b6e !important;
-}
 
 .hljs {
-	color: #000 !important;
-	background: #CED4D4 !important;
+    color: #000 !important;
+    background: #CED4D4 !important;
 }
 
 .hljs-title,
 .hljs-title.class_,
 .hljs-title.class_.inherited__,
 .hljs-title.function_ {
-	color: #693CBE !important;
+    color: #693CBE !important;
 }
+
 .hljs-doctag,
 .hljs-keyword,
 .hljs-meta .hljs-keyword,
@@ -195,18 +228,18 @@ textarea {
 .hljs-template-variable,
 .hljs-type,
 .hljs-variable.language_ {
-	color: #AE2431 !important;
+    color: #AE2431 !important;
 }
 
 .hljs-built_in,
 .hljs-symbol {
-	color: #923F07 !important;
+    color: #923F07 !important;
 }
 
 .hljs-code,
 .hljs-comment,
 .hljs-formula {
-	color: #545454 !important;
+    color: #545454 !important;
 }
 
 .menu_dropdown .menu__item-link {
@@ -218,35 +251,34 @@ textarea {
     font-weight: 700;
     font-size: 14px;
     color: #000;
-    background-color: #e0e0e0; /*#e0e0e0;*/
+    background: #e0e0e0; /*#e0e0e0;*/
     text-decoration: none;
     border-bottom: solid 1px #000;
 }
+
 /* --------------end--------------------- */
 
 
 a.menu_item-link {
-background: red !important}
+    background: red !important
+}
 
-.icons-bar.icons-bar_horizontal{
-    background-color: #e0e0e0 !important;
+.icons-bar.icons-bar_horizontal {
+    background: #e0e0e0 !important;
     border: solid 1px #000;
 }
+
 .icons-bar__item_control {
     color: #506273 !important;
 }
 
-.btn__counter{
+.btn__counter {
     border-left: 1px solid #65c178 !important;
     color: #fff !important;
 }
 
 a.filters-menu__link.filters-menu__link_active {
     border: solid;
-}
-
-.events-list__item.event_navbar {
-    background-color: #505b6e !important;
 }
 
 
@@ -268,15 +300,13 @@ a.filters-menu__link.filters-menu__link_active {
     color: #65c178;
 }
 
-.menu_popup .menu__item, .menu_popup .chzn-results>li {
+.menu_popup .menu__item, .menu_popup .chzn-results > li {
     float: none;
     margin: 0;
     display: block;
     border-bottom: 1px solid #f0f0f0 !important;
     padding: 12px;
-    }
-/* ---------------end----------------- */
-
+}
 
 
 /* ----------------------------------- */
@@ -284,48 +314,38 @@ a.filters-menu__link.filters-menu__link_active {
 /* ----------------------------------- */
 
 .menu_dropdown a.menu__item-link {
-   color: black !important;
-   background-color : #ced4d4 !important;
-   }
+    color: black !important;
+    background: #ced4d4 !important;
+}
 
 a.menu__item-link:hover,
 #abuse_question_link_1396548:hover {
-   background: #eef2f5 !important;
-   }
+    background: #eef2f5 !important;
+}
 
 #abuse_question_link_1396548 {
-background-color : #CED4D4 !important;
+    background: #CED4D4 !important;
 }
+
 /* ---------------end----------------- */
 
-a,h1,.question__views-count,.question__date,.section-header__title {
+a, h1, .question__views-count, .question__date, .section-header__title {
     color: #e0e0e0 !important;
 }
-
 
 
 .question__title-link {
     color: #000;
 }
 
-button {
-    background-color: #505b6e !important;
-    color: #fff !important;
-}
 
 p, div {
-    background-color: #505b6e !important;
+    background: #505b6e !important;
     color: #e0e0e0 !important;
 }
 
 li.content-list__item {
     border-bottom: none;
-}
-
-select {
-    background-color: #333333 !important;
-    color: #fff !important;
-    border-color: #555 !important;
 }
 
 .svg-icon_level {
@@ -338,12 +358,12 @@ select {
 /* ------------------------------------------- */
 /* Меняем значки сложности вопросов на 🟢🟠🔴 */
 /* ------------------------------------------- */
-/* Скрываем оригинальное содержимое */
+/* Скрываем оригинальное содержимое            */
 .svg-icon_level > * {
     display: none !important;
 }
 
-/* Добавляем emoji как псевдоэлемент */
+/* Добавляем emoji как псевдоэлемент           */
 .svg-icon_level::after {
     content: attr(data-emoji);
     position: absolute;
@@ -367,7 +387,7 @@ select {
 
 .svg-icon_level-4::after {
     content: "🔴" !important;
-}
+
     `);
 
     // Создаём метку с временем создания
